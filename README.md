ed11ec875663be820df1669b517a6963ec40a4c4
Tero Frondelius <tero.frondelius@gmail.com>
2019-07-07 01:08:04 +0300

    Add autogenerated files to build gnuplotBuilder

---
+++ README.md
@@ -0,0 +1,9 @@
+# gnuplotBuilder
+
+[![Build Status](https://travis-ci.org/TeroFrondelius/gnuplotBuilder.svg?branch=master)](https://travis-ci.org/TeroFrondelius/gnuplotBuilder)
+
+This repository builds binary artifacts for the gnuplotBuilder project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/TeroFrondelius/gnuplotBuilder/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,78 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "gnuplotBuilder"
+version = v"5.2.7"
+
+# Collection of sources required to build gnuplotBuilder
+sources = [
+    "https://sourceforge.net/projects/gnuplot/files/gnuplot/5.2.7/gnuplot-5.2.7.tar.gz" =>
+    "97fe503ff3b2e356fe2ae32203fc7fd2cf9cef1f46b60fe46dc501a228b9f4ed",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/gnuplot-5.2.7/
+./configure --prefix=$prefix --host=$target
+make
+make install
+
+if [ $target = "x86_64-w64-mingw32" ]; then
+
+fi
+
+if [ $target = "x86_64-w64-mingw32" ]; then
+cd $WORKSPACE/srcdir
+cd ..
+find . -name=so
+find . -n=so
+find . --name=so
+find . -name so
+find . -name gnu
+find -name gnu .
+find . -name ".so"
+find . -name ".so"
+find . -name "gnuplot"
+find . -name "*.so"
+find . -name "so"
+find . -name "o"
+find . -name "gnu"
+find . -name "*gnu*"
+find . -name "*so*"
+find . -name "*dll*"
+
+fi
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:i686, libc=:glibc),
+    Linux(:x86_64, libc=:glibc),
+    Linux(:aarch64, libc=:glibc),
+    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    Linux(:powerpc64le, libc=:glibc),
+    Linux(:i686, libc=:musl),
+    Linux(:x86_64, libc=:musl),
+    Linux(:aarch64, libc=:musl),
+    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
+    MacOS(:x86_64),
+    FreeBSD(:x86_64)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    ExecutableProduct(prefix, "gnuplot", :gnuplot)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
