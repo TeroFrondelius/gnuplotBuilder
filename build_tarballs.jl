8b20c8805f7c093fdb4df1707080dc433e9dc5dc
Tero Frondelius <TeroFrondelius@users.noreply.github.com>
2019-07-07 02:06:56 +0300

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -25,13 +25,13 @@ make install
 platforms = [
     # Linux(:i686, libc=:glibc),
     Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:powerpc64le, libc=:glibc),
+    # Linux(:aarch64, libc=:glibc),
+    # Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    # Linux(:powerpc64le, libc=:glibc),
     # Linux(:i686, libc=:musl),
-    Linux(:x86_64, libc=:musl),
-    Linux(:aarch64, libc=:musl),
-    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
+    # Linux(:x86_64, libc=:musl),
+    # Linux(:aarch64, libc=:musl),
+    # Linux(:armv7l, libc=:musl, call_abi=:eabihf),
     MacOS(:x86_64),
     FreeBSD(:x86_64)
 ]
